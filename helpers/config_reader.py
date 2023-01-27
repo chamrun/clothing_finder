0f732dc61e6d83d3c0d0cf500bc8c65201635fe3
ChamRun <chrm@aut.ac.ir>
2023-01-27 17:04:36 +0330

    Create config_reader.py

---
+++ helpers/config_reader.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+def read_config(file_name, config_path=None, key=None):
+    if config_path is None:
+        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config')
+    config_file = os.path.join(config_path, file_name)
+
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+
+    if key is None:
+        return config
+
+    if key not in config:
+        raise Exception(f'Key {key} not found in config file {file_name}')
+
+    return config[key]
