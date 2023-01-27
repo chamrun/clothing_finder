940005eada23699c0059104da4244716fd3a299f
ChamRun <chrm@aut.ac.ir>
2023-01-27 21:57:40 +0330

    create `categories` var

--- ui/ReactFakeShop/src/components/Products.js
+++ ui/ReactFakeShop/src/components/Products.js
@@ -26,18 +26,22 @@ function Products() {
         getProdcuts();
     }, []);
 
+    let categories = (
+      <div className="position-sticky" style={{ top: "100px" }}>
+        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => setFilter(data)}>All</button>
+        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("1")}>Women's Clothing</button>
+        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("3")}>Men's Clothing</button>
+        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("2")}>Jewelery</button>
+        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("electronics")}>Electronics</button>
+      </div>
+    )
+
     const Loading = () => {
         return (
             <>
                 <div className="col-md-3 my-3">
 
-                    <div className="position-sticky" style={{ top: "100px" }}>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => setFilter(data)}>All</button>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("women's clothing")}>Women's Clothing</button>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("men's clothing")}>Men's Clothing</button>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("jewelery")}>Jewelery</button>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("electronics")}>Electronics</button>
-                    </div>
+                  {categories}
 
                 </div>
 
@@ -63,7 +67,7 @@ function Products() {
                         </div>
                         <div className="col-6 col-md-6 col-lg-4 mb-3">
                             <Skeleton height={400} width={"100%"}/>
-                        </div>  
+                        </div>
                     </div>
 
                 </div>
@@ -79,48 +83,44 @@ function Products() {
     }
 
     const ShowProducts = () => {
-        return (
-            <>
-                <div className="col-md-3 my-3">
 
-                    <div className="position-sticky" style={{ top: "100px" }}>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => setFilter(data)}>All</button>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("women's clothing")}>Women's Clothing</button>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("men's clothing")}>Men's Clothing</button>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("jewelery")}>Jewelery</button>
-                        <button className="btn btn-outline-dark m-1 btn-sm" onClick={() => filterProduct("electronics")}>Electronics</button>
-                    </div>
+      return (
+        <>
+              <div className="col-md-3 my-3">
 
+                {categories}
+
+              </div>
+
+              <div className="col-md-9 py-md-3">
+                <div className="row">
+                  {filter.map((product) => {
+                    return (
+                      <div className="col-6 col-md-6 col-lg-4 mb-3" key={product.id}>
+
+                        <div className="card h-100">
+                          <img src={product.image} className="m-3"
+                               style={{ height: "300px", width: "auto", objectFit: "contain" }} alt={product.title}/>
+                          <div className="m-3 mb-0">
+                            <small className="card-title">{product.title.substring(0, 50)}...</small>
+                          </div>
+                          <div style={{ marginTop: "auto" }}>
+                            <div className="d-flex justify-content-between align-items-center">
+                              <div className="m-3"><b>${product.price}</b></div>
+                              {/*<NavLink className="stretched-link" to={`/product/${product.id}`}>*/}
+                              {/*    <button className="btn btn-sm m-3 border-primary">*/}
+                              {/*        <i className="fa fa-arrow-right text-muted"></i>*/}
+                              {/*    </button>*/}
+                              {/*</NavLink>*/}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  })}
                 </div>
 
-                <div className="col-md-9 py-md-3">
-                    <div className="row">
-                        {filter.map((product) => {
-                            return (
-                                <div className="col-6 col-md-6 col-lg-4 mb-3" key={product.id}>
-
-                                    <div className="card h-100">
-                                        <img src={product.image} className="m-3" style={{ height: "300px", width: "auto", objectFit: "contain" }} alt={product.title} />
-                                        <div className="m-3 mb-0">
-                                            <small className="card-title">{product.title.substring(0, 50)}...</small>
-                                        </div>
-                                        <div style={{ marginTop: "auto" }}>
-                                            <div className="d-flex justify-content-between align-items-center">
-                                                <div className="m-3"><b>${product.price}</b></div>
-                                                {/*<NavLink className="stretched-link" to={`/product/${product.id}`}>*/}
-                                                {/*    <button className="btn btn-sm m-3 border-primary">*/}
-                                                {/*        <i className="fa fa-arrow-right text-muted"></i>*/}
-                                                {/*    </button>*/}
-                                                {/*</NavLink>*/}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-
-                </div>
+              </div>
 
 
             </>
